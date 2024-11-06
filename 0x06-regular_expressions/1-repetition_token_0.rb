8103630f8d1a1b269f2f5a6d2d9b065b30cf38fb
laila <laila.mf2005@gmail.com>
2024-11-06 13:12:49 -0800

    task1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,11 +1,3 @@
 #!/usr/bin/env ruby
 
-input = ARGV[0]
-
-matches = input.scan(/^hb(t+)n$/)
-
-if matches.empty?
-  puts "No match"
-else
-  puts matches.join(" ")
-end
+puts ARGV[0].scan(/^hb(t+)n$/).join
